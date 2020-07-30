f585979c6447a8b3c4dd60f7761bce1c7e46c3ce
valer1435 <valp998@gmail.com>
2020-07-30 21:32:30 +0300

    init

---
+++ help_functions.py
@@ -0,0 +1,81 @@
+import dlib
+import io
+import os
+import traceback
+import subprocess
+from hashlib import md5
+from io import BytesIO
+import cv2
+import requests
+import numpy as np
+
+
+NEW_SAMPLERATE = "16000"
+FFMPEG_PATH = "bin/ffmpeg.exe"
+AUDIO_FOLDER = "audio"
+PHOTOS_FOLDER = "photos"
+detector = dlib.get_frontal_face_detector()
+
+
+def convert_voice_file(old_file_name):
+    new_file_name = old_file_name.replace(".ogg", ".wav")
+    subprocess.call(
+        [FFMPEG_PATH,
+         "-i", old_file_name,
+         "-ar", NEW_SAMPLERATE,
+         new_file_name],
+        stderr=subprocess.DEVNULL)
+    os.remove(old_file_name)
+    return new_file_name
+
+
+def save_voice_file(file, user_id):
+    try:
+        user_id_hash = get_md5(str(user_id))
+        file_name = f"{AUDIO_FOLDER+os.sep+user_id_hash+os.sep+file['file_id']}.ogg"
+        if not os.path.exists(AUDIO_FOLDER):
+            os.mkdir(AUDIO_FOLDER)
+        if not os.path.exists(AUDIO_FOLDER+os.sep+user_id_hash):
+            os.mkdir(AUDIO_FOLDER+os.sep+user_id_hash)
+        file.download(file_name)
+        convert_voice_file(file_name)
+        return True
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        return False
+
+
+def process_photo(file):
+    try:
+        raw_bytes = requests.get(file.file_path).content
+        faces_count, img = detect_faces(raw_bytes)
+        if faces_count:
+            save_photo(file)
+            return faces_count, io.BufferedReader(BytesIO(img))
+        return faces_count, io.BufferedReader(BytesIO(raw_bytes))
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        return 0, None
+
+
+def save_photo(file):
+    if not os.path.exists(PHOTOS_FOLDER):
+        os.mkdir(PHOTOS_FOLDER)
+    file.download(f"{PHOTOS_FOLDER+os.sep+file.file_id}.png")
+    return True
+
+
+def detect_faces(raw_photo):
+    image = np.asarray(bytearray(raw_photo), dtype="uint8")
+    image_bgr = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    faces = detector(image_rgb, 1)
+    faces_count = 0
+    for i, d in enumerate(faces):
+        image_bgr = cv2.rectangle(image_bgr, (d.left(), d.top()), (d.right(), d.bottom()), (0, 255, 0), 3)
+        faces_count += 1
+    return faces_count, cv2.imencode('.png', image_bgr)[1]
+
+
+def get_md5(in_string):
+    return md5(in_string.encode()).hexdigest()
---
+++ main_bot.py
@@ -0,0 +1,81 @@
+import random
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+from help_functions import *
+from important_constants import API_TOKEN
+
+
+def get_voice_file(update, context):
+    chat_id = update.message.chat.id
+    file = context.bot.getFile(update.message.voice.file_id)
+    if save_voice_file(file, str(chat_id)):
+        context.bot.send_message(chat_id=chat_id, text="Успешно загрузил голосовое сообщение в .wav формат")
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Произошла ошибка обработки аудиосообщения")
+
+
+def get_random_voice(update, context):
+    chat_id = update.message.chat.id
+    chat_id_hash = get_md5(str(chat_id))
+    if not os.path.exists(AUDIO_FOLDER):
+        context.bot.send_message(chat_id=chat_id, text="Вы еще не отправили ни одного голосового сообщения")
+        return
+    if not os.path.exists(AUDIO_FOLDER+os.sep+chat_id_hash):
+        context.bot.send_message(chat_id=chat_id, text="Вы еще не отправили ни одного голосового сообщения")
+        return
+    if not os.listdir(AUDIO_FOLDER+os.sep+chat_id_hash):
+        context.bot.send_message(chat_id=chat_id, text="Нет подходящих сообщений")
+        return
+    path = AUDIO_FOLDER+os.sep+chat_id_hash+os.sep+random.choice(os.listdir(AUDIO_FOLDER+os.sep+chat_id_hash))
+    context.bot.send_voice(chat_id=chat_id, voice=open(path, 'rb'))
+
+
+def my_help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="""
+                             Я бот! Отправьте мне голосовое сообщение и я сохраню его!
+                             Отправьте мне фотографию и я определю, есть ли на ней лица!
+                             /myVoice - отправить случайное записанное голосовое сообщение
+                             /start - старт
+                             /help - помощь""")
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Я бот! Отправьте мне голосовое сообщение или фотографию (/help - помощь)")
+
+
+def evaluate_photos(update, context):
+    photo = context.bot.getFile(update.message.photo[-1].file_id)
+    faces_count, photo = process_photo(photo)
+    if faces_count:
+        context.bot.send_message(update.message.chat.id, f"Кол-во лиц на этой фотографии: {faces_count}, сохраняю")
+        context.bot.send_photo(update.message.chat.id, photo)
+    elif photo is None:
+        context.bot.send_message(update.message.chat.id, "Произошла ошибка обработки изображения")
+    else:
+        context.bot.send_message(update.message.chat.id, "На этой фотографии нет лиц-не сохраняю")
+        context.bot.send_photo(update.message.chat.id, photo)
+
+
+updater = Updater(token=API_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+help_handler = CommandHandler('help', my_help)
+dispatcher.add_handler(help_handler)
+
+get_random_voice_handler = CommandHandler('myVoice', get_random_voice)
+dispatcher.add_handler(get_random_voice_handler)
+
+voice_handler = MessageHandler(Filters.voice,  get_voice_file)
+dispatcher.add_handler(voice_handler)
+
+photo_handler = MessageHandler(Filters.photo,  evaluate_photos)
+dispatcher.add_handler(photo_handler)
+
+updater.start_polling()
